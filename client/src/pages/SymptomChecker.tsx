2115be43ba955d335be9bd91e2336b27b1a7239c
Dineshkumar2006471 <bingidineshkumar@gmail.com>
2025-11-30 14:16:44 +0530

    Update Symptom Checker results to display in full width single column layout

--- client/src/pages/SymptomChecker.tsx
+++ client/src/pages/SymptomChecker.tsx
@@ -236,7 +236,7 @@ export default function SymptomChecker() {
                                 </div>
                             </div>
 
-                            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                            <div className="grid grid-cols-1 gap-8">
                                 {/* Left Column: Warning Signs & Recommendation */}
                                 <div className="space-y-6">
                                     {/* Warning Signs */}
